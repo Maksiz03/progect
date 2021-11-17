4f8912e9ea2eca9bbe52e53bc98b3edafef797a7
Maksiz03 <92305927+Maksiz03@users.noreply.github.com>
2021-11-18 00:11:02 +0500

    Create фибаначи.cs

---
+++ фибаначи.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static int metodFiban2(int i)
+        {
+            int result = 0;
+            int b = 1;
+            int tmp;
+
+            for (int j = 0; j < i; j++)
+            {
+                tmp = result;
+                result = b;
+                b += tmp;
+            }
+
+            return result;
+        }
+        public static int metodFiban(int i)
+        {
+            int number1, number2;
+            if (i <= 2)
+                return 1;
+            else
+            {
+                number1 = metodFiban(i - 1);
+                number2 = metodFiban(i - 2);
+            }
+            return number1 + number2;
+            
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(metodFiban2(100));
+        }
+    }
+}
