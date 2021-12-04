5ea3250ea9d84246b36a16be9ef52051bab89d27
Maksiz03 <92305927+Maksiz03@users.noreply.github.com>
2021-12-04 16:11:33 +0500

    Add files via upload

---
+++ ооп.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class UniversityPeople
+    {
+        protected string name;
+        protected double yearsInUni;
+        public function constructor() { }
+
+    }
+    class Student :UniversityPeople
+    {
+        protected int numberGroup;
+        sealed public override void money()
+        {
+            Console.WriteLine("стипендия");
+        }
+    }
+    class Teacher : UniversityPeople
+    {
+        public int numberOfStudents;
+        sealed public override void money()
+        {
+            Console.WriteLine("зарплата");
+        }
+    }
+}
